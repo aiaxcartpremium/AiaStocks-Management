2c7bdbba2eda7f2eee8da2c60481536396648505
aiaxcartpremium <shanaiamau99@gmail.com>
2025-11-05 01:16:56 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 
+export const dynamic = 'force-dynamic'
+
 export const metadata = {
   title: 'Aiaxcart Premium Shop',
   description: 'Stocks • Records • Admin • Owner'
